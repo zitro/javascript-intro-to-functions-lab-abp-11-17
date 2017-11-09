cfa522db8bc1a79d2751eb6223113bc0de3eab97
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 18:05:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string){
+  string.toUpperCase;
+}
+function whisper(string){
+  string.toLowerCase;
+}
+function logShout(string){
+  console.log(string.toUpperCase);
+}
+function logWhisper(string){
+  console.log(string.toLowerCase);
+}
+function sayHiToGrandma(string){
+if(string === 'I love you, Grandma.')
+  return " I love you, too.";
+}else if(string === whisper){
+  return " I can't hear you!";
+}else if(string === shout){
+  return 'YES INDEED!';
+}
+};
